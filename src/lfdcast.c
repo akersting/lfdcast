b92459b11b5556fbfd94905dbce50a96fb9aef9d
Andreas Kersting <andreas.kersting@gfk.com>
2019-08-20 23:03:17 +0200

    hack to work with R < v3.5.0

--- src/lfdcast.c
+++ src/lfdcast.c
@@ -1,3 +1,9 @@
+// required for DATAPTR
+#include <Rversion.h>
+#if defined(R_VERSION) && R_VERSION < R_Version(3, 5, 0)
+#define USE_RINTERNALS
+#endif
+
 #include "Rinternals.h"
 #include <pthread.h>
 
