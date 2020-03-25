2e398f541ac91c93e610875b2eb6eee325dd9241
Andreas Kersting <andreas.kersting@gfk.com>
2020-03-25 22:19:03 +0100

    restructured char_map code

--- src/char_map.c
+++ src/char_map.c
@@ -2,67 +2,81 @@
 #include "rsort.h"
 
 SEXP char_map(SEXP x) {
-  const void *restrict input = DATAPTR(x);
-  int n = LENGTH(x);
+  SEXP unique;
+  SEXP map;
+
+  const int n = LENGTH(x);
+
+  if (n == 0) {
+    unique = PROTECT(allocVector(STRSXP, 0));
+    map = PROTECT(allocVector(INTSXP, 0));
+    goto end;
+  }
+
+  const intptr_t *restrict input = (intptr_t *) DATAPTR(x);
 
   struct uniqueN_data * restrict uniqueN_data =
     (struct uniqueN_data *) malloc(n * sizeof(struct uniqueN_data));
 
-  int (*restrict hist_rank)[n_bucket] = malloc(sizeof(int[n_pass_rank][n_bucket]));
   int (*restrict hist_value)[n_bucket] = malloc(sizeof(int[n_pass_value][n_bucket]));
-  memset(hist_rank, 0, n_pass_rank * n_bucket * sizeof(int));
   memset(hist_value, 0, n_pass_value * n_bucket * sizeof(int));
 
-  int uniqueN_data_length = 0;
-  for (int i = 0; i < n; i++) {
-    (uniqueN_data + uniqueN_data_length)->rank = i;
-    (uniqueN_data + uniqueN_data_length)->value = ((intptr_t *) input)[i];
+  for (uint32_t i = 0; i < n; i++) {
+    (uniqueN_data + i)->rank = i;
+    (uniqueN_data + i)->value = input[i];
 
+    //for (int jj = 0; jj < n_pass_value; jj++) {
+    //  hist_value[jj][(uniqueN_data + i)->value >> jj * shift & mask]++;
+    //}
 
-    for (int jj = 0; jj < n_pass_rank; jj++) {
-      hist_rank[jj][(uniqueN_data + uniqueN_data_length)->rank >> jj * shift & mask]++;
-    }
-    for (int jj = 0; jj < n_pass_value; jj++) {
-      hist_value[jj][(uniqueN_data + uniqueN_data_length)->value >> jj * shift & mask]++;
-    }
-
-    uniqueN_data_length++;
+    _Static_assert(n_pass_value == 8, "n_pass_value must be 8");
+    hist_value[0][input[i] >> 0 * shift & mask]++;
+    hist_value[1][input[i] >> 1 * shift & mask]++;
+    hist_value[2][input[i] >> 2 * shift & mask]++;
+    hist_value[3][input[i] >> 3 * shift & mask]++;
+    hist_value[4][input[i] >> 4 * shift & mask]++;
+    hist_value[5][input[i] >> 5 * shift & mask]++;
+    hist_value[6][input[i] >> 6 * shift & mask]++;
+    hist_value[7][input[i] >> 7 * shift & mask]++;
   }
 
-  SEXP map = PROTECT(allocVector(INTSXP, n));
-  SEXP unique;
-  int unique_cntr = 0;
+  rsort(uniqueN_data, n, NULL, hist_value, VALUE_THEN_RANK);
+  free(hist_value);
 
-  if (uniqueN_data_length > 0) {
-    rsort(uniqueN_data, uniqueN_data_length, hist_rank, hist_value, VALUE_THEN_RANK);
+  map = PROTECT(allocVector(INTSXP, n));
+  int *restrict map_ptr = INTEGER(map);
+  int unique_cntr = 1;
+  map_ptr[uniqueN_data->rank] = unique_cntr;
 
-    unique_cntr = 1;
-    INTEGER(map)[uniqueN_data->rank] = unique_cntr;
+  for (int i = 1, ii = 0; i < n; i++) {
+    /*if ((uniqueN_data + i)->value != (uniqueN_data + i - 1)->value) {
+      unique_cntr++;
+      map_ptr[(uniqueN_data + i)->rank] = unique_cntr;
+      (uniqueN_data + ii)->rank = i;
+      ii = i;
+    } else {
+      map_ptr[(uniqueN_data + i)->rank] = unique_cntr;
+    }*/
 
-    for (int i = 1; i < uniqueN_data_length; i ++) {
-      if ((uniqueN_data + i)->value != (uniqueN_data + i - 1)->value) {
-        unique_cntr++;
-      }
-      INTEGER(map)[(uniqueN_data + i)->rank] = unique_cntr;
+    if ((uniqueN_data + i)->value != (uniqueN_data + i - 1)->value) {
+      unique_cntr++;
+      (uniqueN_data + ii)->rank = i;
+      ii = i;
     }
+    map_ptr[(uniqueN_data + i)->rank] = unique_cntr;
+  }
 
-    unique = PROTECT(allocVector(STRSXP, unique_cntr));
-    SET_STRING_ELT(unique, 0, (SEXP) uniqueN_data->value);
+  unique = PROTECT(allocVector(STRSXP, unique_cntr));
+  SET_STRING_ELT(unique, 0, (SEXP) uniqueN_data->value);
 
-    unique_cntr = 0;
-    for (int i = 1; i < uniqueN_data_length; i ++) {
-      if ((uniqueN_data + i)->value != (uniqueN_data + i - 1)->value) {
-        SET_STRING_ELT(unique, ++unique_cntr, (SEXP) (uniqueN_data + i)->value);
-      }
-    }
-  } else {
-    unique = PROTECT(allocVector(STRSXP, 0));
+  for (int i = 1, ii = 0; i < unique_cntr; i++) {
+    ii = (uniqueN_data + ii)->rank;
+    SET_STRING_ELT(unique, i, (SEXP) (uniqueN_data + ii)->value);
   }
 
   free(uniqueN_data);
-  free(hist_rank);
-  free(hist_value);
 
+  end: ;
   SEXP ret = PROTECT(allocVector(VECSXP, 2));
   SET_VECTOR_ELT(ret, 0, unique);
   SET_VECTOR_ELT(ret, 1, map);
